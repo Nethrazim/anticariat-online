eb6e4df320ed325a1891b30b32989f154dd8cc0d
Mavonim <Mavonim@DESKTOP-0MOALKC>
2020-09-29 15:37:47 +0300

    Increased filter fields width

---
+++ app/javascript/components/views/BooksView/BooksWithPriceReductions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class BooksWithPriceReductions extends React.Component
+{
+
+    render()
+    {
+        return(<div className="container books_with_price_reductions_page">
+            <p>TODO: Price Reductions</p>
+        </div>);
+    }
+}
+
+
+
+export default BooksWithPriceReductions;
